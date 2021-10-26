10d07b4dc6269f44e3ec26fe2a1b3d1d45d39eac
meiyokraken <loic.demesmay@gmail.com>
2021-10-26 12:12:20 +0200

    Update index.php
    add test data

--- index.php
+++ index.php
@@ -3,12 +3,26 @@
 use app\model\repository\ItemRepository;
 require_once (__DIR__."/src/model/repository/ItemRepository.php");
 
-$itemToCreate = array(
-    "title" => "My List Title",
-    "content" => "My list content",
-    "isDone" => false
-);
-
 $itemRepository = new ItemRepository();
 
-$itemRepository->insert($itemToCreate);
+//simulate input
+for($i = 0; $i < 10; $i++) {
+    $itemToCreate = array(
+        "title" => "My List Title " . $i,
+        "content" => "My list content" .$i,
+        "isDone" => false
+    );
+
+    $itemRepository->insert($itemToCreate);
+}
+
+//must return a list of pushed items
+//$lists = $itemRepository->getAll();
+
+//debug print all title from pushed items, if exist;
+function printAll($items) {
+    foreach($items as $item) {
+        echo $item->getTitle();
+    }
+}
+
